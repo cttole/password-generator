db68ac516e26d1e1b4c7803a7704e24d38f6c76c
Tuckers files <christole@chrissmacstudio.lan>
2023-06-13 15:30:01 -0500

    completely refactored javascript code to add in choose your own length

--- Develop/script.js
+++ Develop/script.js
@@ -1,54 +1,52 @@
-// Assignment code here
 
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var length8Btn = document.createElement("#button");
-var length16Btn = document.createElement("#button");
-var length32Btn = document.createElement("#button");
 var passwordText = document.querySelector("#password");
 
-length8Btn.textContent = "8";
-length16Btn.textContent = "16";
-length32Btn.textContent = "32";
-
-
+var includeLowercase, includeUppercase, includeNumeric, includeSpecial;
 
 function generatePassword(length) {
-  var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+";
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()-_=+";
+  var characters = "";
   var password = "";
+
+  if (includeLowercase) {
+    characters += lowercaseChars;
+  }
+  if (includeUppercase) {
+    characters += uppercaseChars;
+  }
+  if (includeNumeric) {
+    characters += numericChars;
+  }
+  if (includeSpecial) {
+    characters += specialChars;
+  }
+
   for (var i = 0; i < length; i++) {
     var randomIndex = Math.floor(Math.random() * characters.length);
     password += characters.charAt(randomIndex);
   }
   return password;
-
 }
+
 function handleGenerateClick() {
-  generateBtn.remove();
+  var passwordLength = prompt("Enter desired password length (8-128)");
 
-  length8Btn.classList.add("btn");
-  length16Btn.classList.add("btn");
-  length32Btn.classList.add("btn");
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    includeLowercase = confirm("Include lowercase characters");
+    includeUppercase = confirm("Include Uppercase characters");
+    includeNumeric = confirm("Include Numbers");
+    includeSpecial = confirm("Include special characters");
 
-  var cardFooter = document.querySelector(".card-footer");
-    cardFooter.appendChild(length8Btn);
-    cardFooter.appendChild(length16Btn);
-    cardFooter.appendChild(length32Btn);
+    var password = generatePassword(passwordLength);
+
+    passwordText.value = password;
+  } else {
+    alert("Hey silly, the password length should be between 8 and 128.");
+  }
 }
-function handleLengthClick(event){
-var passwordLength = parseInt(event.target.textContent);
-var password = generatePassword(passwordLength)
-passwordText.value = password;
-}
 
-
-
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", handleGenerateClick);
-length8Btn.addEventListener("click", handleLengthClick);
-length16Btn.addEventListener("click", handleLengthClick);
-length32Btn.addEventListener("click", handleLengthClick);
- 
-
